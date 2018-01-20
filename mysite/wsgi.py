f7e5616bb3f2c162334255ecc15f2224d0c247dd
Matej Dujava <mdujava@kocurkovo.cz>
2018-01-20 14:25:09 +0100

    first view and template
    Signed-off-by: Matej Dujava <mdujava@kocurkovo.cz>

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+path = os.path.expanduser('~/my-first-blog')
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
